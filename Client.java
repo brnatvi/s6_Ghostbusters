10b46cddee7e307ec2f283fd12f4f911d7598a6e
tharsi <tharsi@tharsi-VivoBook-ASUSLaptop-X411UAO>
2022-04-09 15:01:17 +0200

    client get message with a simple read

--- Client.java
+++ Client.java
@@ -5,69 +5,12 @@ import java.util.Scanner;
 class Client {
 
     //close correctement
-    private BufferedReader in;
-    private PrintWriter out;
-    private Socket sockfd;
-    private Scanner sc = new Scanner(System.in);
+    BufferedReader in;
+    PrintWriter out;
+    Socket sockfd;
+    Scanner sc = new Scanner(System.in);
 
-
-    public void getMessage(){
-        Thread get = new Thread(new Runnable() {
-            char recu;
-            int stars = 0;
-
-            @Override
-            public void run(){
-                try {
-                    while((recu = (char)in.read()) != '*' || stars != 3){ 
-                        System.out.print(recu);
-                        if(recu == '*') {
-                            stars ++;   
-                        }
-                        if(stars == 3 && in.ready()){ //Si on a trois étoiles mais qu'il reste encore à lire, on continue en réinitialisant stars à 0
-                            stars = 0;
-                        }
-                    }
-                    stars = 0;
-                    
-                    System.out.println();
-                    
-                }
-                catch(Exception e){
-                    e.printStackTrace();
-                }
-            }
-
-        });
-        get.start();
-
-    }
-
-    public void sendMessage(){
-        Thread send = new Thread(new Runnable(){
-            String msg;     
-            
-            @Override
-            public void run(){
-                
-                while(true){
-                    msg = sc.nextLine();
-                    out.print(msg);
-                    out.flush();
-                } 
-            }
-
-        });
-        send.start();
-    }
-
-    public void communication(){
-       
-        getMessage();
-        sendMessage();
-        
-    }
-
+    //Connexion TCP du client au serveur
     public Client(String adresse, String port){
         try {
             sockfd = new Socket(adresse, Integer.parseInt(port));
@@ -82,8 +25,62 @@ class Client {
         }
     }
 
+    //Lire buffer
+    public String read(BufferedReader in, char[] buf, int offset, int len){
+        try{
+            in.read(buf, offset, len);
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        return (new String(buf)).trim();
+    }
+
+    //récupérer message avec peu d'appels systemes
+    public void getMessage(BufferedReader in){
+        String msg = "";
+        char[] buf = new char[1024];
+        msg = read(in, buf, 0, 1024);
+        int begin = 5;
+        String en_tete = msg.substring(0,5);
+        if(en_tete.equals("GAMES")){
+
+        }
+        else if(en_tete.equals("REGOK")){
+            
+        }
+        
+        else if(en_tete.equals("REGNO")){
+            System.out.println(en_tete + msg.substring(begin, begin+3));            
+        }
+        else if(en_tete.equals("UNROK")){
+
+        }
+        else if(en_tete.equals("SIZE!")){
+            
+        }
+        else if(en_tete.equals("LIST!")){
+            
+        }   
+        else if(en_tete.equals("PLAYR")){
+            
+        }
+        else if(en_tete.equals("DUNNO")){
+            System.out.println(en_tete + msg.substring(begin, begin+3));
+        }     
+    }
+
+    //envoie un message
+    public void sendMessage(PrintWriter out){
+        String msg = sc.nextLine();
+        out.print(msg);
+        out.flush();
+    }
+
     public static void main(String[] args){
-        Client p = new Client("localhost", "4242");
-        p.communication();
+        Client p = new Client(args[0], args[1]);
+        p.getMessage(p.in);
+        p.sendMessage(p.out);
+        
     }
 }

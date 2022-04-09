35508fab09c59f1e27659f07fdf15c5d14bdd3c6
tharsi <tharsi@tharsi-VivoBook-ASUSLaptop-X411UAO>
2022-04-09 16:49:09 +0200

    client tcp connection, receive and send messages to server, read until finding three stars and nothing after

--- Client.java
+++ Client.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class Client {
 
-    //close correctement
+    //TODO: close correctement
     BufferedReader in;
     PrintWriter out;
     Socket sockfd;
@@ -33,7 +33,35 @@ class Client {
         catch(IOException e){
             System.out.println(e);
         }
-        return (new String(buf)).trim();
+        return (new String(buf)); 
+    }
+
+    //parser jusqu'à trouver '***'
+    public String parseUntilStars(String msg){
+        int ind;
+        int stars = 0;
+        for(ind = 0; ind < msg.length(); ind++){
+            try {
+                if(
+                    (msg.charAt(ind) == '*') && 
+                    (msg.charAt(ind) == msg.charAt(ind+1) && msg.charAt(ind+1) == msg.charAt(ind+2)) && 
+                    (!msg.substring(ind+3).isEmpty()) 
+                ) {
+                    return msg.substring(0,ind+3)+parseUntilStars(msg.substring(ind+3));
+                }
+                else if(
+                    (msg.charAt(ind) == '*') && 
+                    (msg.charAt(ind) == msg.charAt(ind+1) && msg.charAt(ind+1) == msg.charAt(ind+2)) 
+                ){
+                    
+                    return msg.substring(0,ind+3);
+                }
+            }
+            catch(Exception e){
+
+            } 
+        }
+        return "";
     }
 
     //récupérer message avec peu d'appels systemes
@@ -43,31 +71,32 @@ class Client {
         msg = read(in, buf, 0, 1024);
         int begin = 5;
         String en_tete = msg.substring(0,5);
-        if(en_tete.equals("GAMES")){
-
-        }
-        else if(en_tete.equals("REGOK")){
+        System.out.print(en_tete+parseUntilStars(msg.substring(begin)));
+        // if(en_tete.equals("GAMES")){
+        //     System.out.print(en_tete+parseUntilStars(msg.substring(begin)));
+        // }
+        // else if(en_tete.equals("REGOK")){
             
-        }
+        // }
         
-        else if(en_tete.equals("REGNO")){
-            System.out.println(en_tete + msg.substring(begin, begin+3));            
-        }
-        else if(en_tete.equals("UNROK")){
+        // else if(en_tete.equals("REGNO")){
+        //     System.out.print(en_tete+parseUntilStars(msg.substring(begin)));
+        // }
+        // else if(en_tete.equals("UNROK")){
 
-        }
-        else if(en_tete.equals("SIZE!")){
+        // }
+        // else if(en_tete.equals("SIZE!")){
             
-        }
-        else if(en_tete.equals("LIST!")){
+        // }
+        // else if(en_tete.equals("LIST!")){
             
-        }   
-        else if(en_tete.equals("PLAYR")){
+        // }   
+        // else if(en_tete.equals("PLAYR")){
             
-        }
-        else if(en_tete.equals("DUNNO")){
-            System.out.println(en_tete + msg.substring(begin, begin+3));
-        }     
+        // }
+        // else if(en_tete.equals("DUNNO")){
+        //     System.out.println(en_tete + msg.substring(begin, begin+3));
+        // }     
     }
 
     //envoie un message
@@ -79,8 +108,11 @@ class Client {
 
     public static void main(String[] args){
         Client p = new Client(args[0], args[1]);
-        p.getMessage(p.in);
-        p.sendMessage(p.out);
+        while(true){
+            p.getMessage(p.in);
+            p.sendMessage(p.out);
+        }
+
         
     }
 }
